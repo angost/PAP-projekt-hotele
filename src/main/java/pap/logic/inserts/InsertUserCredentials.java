136a36712aac571ca82ce3a2a85d73ad73bccdb5
Adrian Murawski <01178568@pw.edu.pl>
2023-12-12 00:49:15 +0100

    changed inserting user credentials

--- src/main/java/pap/logic/inserts/InsertUserCredentials.java
+++ src/main/java/pap/logic/inserts/InsertUserCredentials.java
@@ -1,15 +1,15 @@
 package pap.logic.inserts;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import pap.db.SessionFactoryMaker;
 import pap.db.entities.*;
+import pap.db.dao.*;
 
 import java.time.LocalDate;
 
 public class InsertUserCredentials {
     private final Client user;
-    private final Address userAddress;
+    private Address userAddress;
+
+    boolean address;
 
     public InsertUserCredentials(String username, String password, String name, String surname, String email, String phoneNumber,
                                  String country, String city, String street, String postalCode, String streetNo,
@@ -32,17 +32,31 @@ public class InsertUserCredentials {
         user.setGender(gender);
         user.setAddress(userAddress);
         user.setActive(activeStatus);
+        address = true;
     }
 
-    public boolean insertIntoDatabase() {
-        try (SessionFactory factory = SessionFactoryMaker.getFactory(); Session session = factory.openSession()) {
-            session.beginTransaction();
-            session.persist(userAddress);
-            session.persist(user);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            return false;
+    public InsertUserCredentials(String username, String password, String name, String surname, String email, String phoneNumber,
+                                 LocalDate birthDate, String nationality, String gender, boolean activeStatus) {
+        user = new Client();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setBirthDate(birthDate);
+        user.setNationality(nationality);
+        user.setGender(gender);
+        user.setActive(activeStatus);
+        address = false;
+    }
+
+    public void insertIntoDatabase() {
+        if (address){
+            new ClientDAO().createWithNewAddress(user);
+        } else {
+            new ClientDAO().create(user);
         }
+
     }
 }
