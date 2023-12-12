358e7ec5048da2c7df9ee779509079391fa5c284
Angelika Ostrowska <01178572@pw.edu.pl>
2023-12-12 16:56:15 +0100

    Added log in buttons and displaying error codes

--- src/main/java/pap/gui/LogInGUI.java
+++ src/main/java/pap/gui/LogInGUI.java
@@ -2,15 +2,20 @@ package pap.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+
+import pap.db.entities.Client;
+import pap.db.entities.Owner;
+import pap.logic.login.*;
 
 
 public class LogInGUI extends BaseGUI {
 
     JPanel mainPanel, centerPanel, loginPanel, createAccountPanel;
     LogoPanel logoPanel;
-    JLabel usernameLabel, createAccountLabel, passwordLabel;
+    JLabel usernameLabel, createAccountLabel, passwordLabel, statusLabel;
     JTextField usernameInputField, passwordInputField;
-    RoundedButton logInButton, createAccountButton;
+    RoundedButton logInClientButton, logInOwnerButton, createAccountButton;
 
     void createCustomGUI(){
         mainPanel = new JPanel();
@@ -27,14 +32,14 @@ public class LogInGUI extends BaseGUI {
 
         centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.LINE_AXIS));
-        centerPanel.setPreferredSize(new Dimension(frameWidth, frameHeight - frameHeight/5 - 2*btnHeight));
-        centerPanel.setMaximumSize(new Dimension(frameWidth, frameHeight - frameHeight/5 - 2*btnHeight));
+        centerPanel.setPreferredSize(new Dimension(frameWidth, frameHeight - frameHeight/5 - 3*btnHeight));
+        centerPanel.setMaximumSize(new Dimension(frameWidth, frameHeight - frameHeight/5 - 3*btnHeight));
         centerPanel.setBackground(bgColor);
         mainPanel.add(centerPanel);
         loginPanel = new JPanel();
         loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.PAGE_AXIS));
-        loginPanel.setPreferredSize(new Dimension(textFieldWidth, frameHeight - frameHeight/5 - 2*btnHeight));
-        loginPanel.setMaximumSize(new Dimension(textFieldWidth, frameHeight - frameHeight/5 -2*btnHeight));
+        loginPanel.setPreferredSize(new Dimension(textFieldWidth, frameHeight - frameHeight/5 - 3*btnHeight));
+        loginPanel.setMaximumSize(new Dimension(textFieldWidth, frameHeight - frameHeight/5 -3*btnHeight));
         loginPanel.setBackground(bgColor);
         loginPanel.add(Box.createVerticalGlue());
 
@@ -60,14 +65,28 @@ public class LogInGUI extends BaseGUI {
         passwordInputField.setPreferredSize(new Dimension(frameWidth, 30));
         passwordInputField.setMaximumSize(new Dimension(frameWidth, 30));
         loginPanel.add(passwordInputField);
-        loginPanel.add(Box.createRigidArea(new Dimension(0,10)));
+        loginPanel.add(Box.createRigidArea(new Dimension(0,20)));
 
         centerPanel.add(Box.createHorizontalGlue()); centerPanel.add(loginPanel); centerPanel.add(Box.createHorizontalGlue());
 
-        logInButton = new RoundedButton("Log in", frameWidth*3/20, btnHeight, secondColor, secondColorDarker, fontButtons, false);
-        logInButton.addActionListener(e->logInBtnClickedAction());
-        loginPanel.add(logInButton);
-        loginPanel.add(Box.createVerticalGlue());
+
+        JPanel buttonsPanel = new JPanel();
+        logInClientButton = new RoundedButton("Log in as Client", frameWidth*3/20, btnHeight, secondColor, secondColorDarker, fontButtons, false);
+        logInClientButton.addActionListener(e->logInClientClickedAction());
+        loginPanel.add(logInClientButton);
+        loginPanel.add(Box.createRigidArea(new Dimension(0,20)));
+        logInOwnerButton = new RoundedButton("Log in as Owner", frameWidth*3/20, btnHeight, secondColor, secondColorDarker, fontButtons, false);
+        logInOwnerButton.addActionListener(e->logInOwnerClickedAction());
+        loginPanel.add(logInOwnerButton);
+
+        statusLabel = new JLabel("<html>Insert your data</html>", JLabel.LEFT);
+        statusLabel.setFont(fontSmaller);
+        statusLabel.setForeground(Color.decode("#7a7373"));
+        loginPanel.add(statusLabel);
+
+        mainPanel.add(Box.createVerticalGlue());
+
+
 
         createAccountPanel = new JPanel();
         createAccountPanel.setLayout(new BoxLayout(createAccountPanel, BoxLayout.LINE_AXIS));
@@ -86,22 +105,41 @@ public class LogInGUI extends BaseGUI {
 
     }
 
-    void logInBtnClickedAction() {
+    void logInClientClickedAction() {
         String usernameText = usernameInputField.getText();
         String passwordText = passwordInputField.getText();
-        boolean dataValid = true; //tyczmaczowo, docelowo funkcja
-        if (usernameText.isEmpty() || passwordText.isEmpty()){
-            dataValid = false;
-        }
 
-        if (dataValid) {
+        // Found user account
+        UserLogin ul = new UserLogin(usernameText, passwordText);
+        Client user = ul.getUserAccount();
+        List<Integer> errorCodesUser = ul.getErrorCodes();
+
+        if (errorCodesUser.isEmpty()) {
             new HomePageGUI().createGUI();
             frame.setVisible(false);
-            // go to home page
         } else {
-            JOptionPane.showMessageDialog(frame, "Couldn't log in");
+            statusLabel.setText(String.valueOf(errorCodesUser.get(0)));
         }
     }
+
+    void logInOwnerClickedAction() {
+        String usernameText = usernameInputField.getText();
+        String passwordText = passwordInputField.getText();
+
+        // Found user account
+        OwnerLogin ol = new OwnerLogin(usernameText, passwordText);
+        Owner owner = ol.getOwnerAccount();
+        List<Integer> errorCodesOwner = ol.getErrorCodes();
+
+        if (errorCodesOwner.isEmpty()) {
+            new HomePageGUI().createGUI();
+            frame.setVisible(false);
+        } else {
+            statusLabel.setText(String.valueOf(errorCodesOwner.get(0)));
+        }
+
+    }
+
 
     void createAccountBtnClickedAction() {
         new ChooseAccountTypeGUI().createGUI();
