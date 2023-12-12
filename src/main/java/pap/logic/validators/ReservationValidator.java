57c35670e768b8165b869e5791c3c8e034c58ab4
Kacper Straszak <01178602@pw.edu.pl>
2023-12-12 11:13:19 +0100

    checkDateAvailable added to ReservationValidator

--- src/main/java/pap/logic/validators/ReservationValidator.java
+++ src/main/java/pap/logic/validators/ReservationValidator.java
@@ -1,26 +1,27 @@
 package pap.logic.validators;
 
+import java.time.LocalDate;
 import java.util.*;
 import pap.db.dao.*;
-import pap.db.entities.Offer;
+import pap.db.entities.*;
 
 public class ReservationValidator {
 
-    private final String dateStart;
-    private final String dateEnd;
+    private final LocalDate dateStart;
+    private final LocalDate dateEnd;
     private final Integer offerId;
-    private final Integer userId;
 
-    public ReservationValidator(String dateStart, String dateEnd, Integer offerId, Integer userId){
+    public ReservationValidator(LocalDate dateStart, LocalDate dateEnd, Integer offerId){
         this.dateStart = dateStart;
         this.dateEnd = dateEnd;
         this.offerId = offerId;
-        this.userId = userId;
     }
 
     public List <Integer> validate(){
         List <Integer> codes = new ArrayList<>();
         checkOfferActive(offerId, codes);
+        checkDateAvailable(offerId, dateStart, dateEnd, codes);
+        return codes;
     }
 
     private static void checkOfferActive(Integer offerId, List <Integer> codes){
@@ -32,4 +33,15 @@ public class ReservationValidator {
         } catch (Exception ignored) {}
     }
 
+    private static void checkDateAvailable(Integer offerId, LocalDate dateStart, LocalDate dateEnd, List<Integer> codes){
+        try {
+            List<Reservation> reservations = new ReservationDAO().findByOfferId(offerId);
+            for (Reservation reservation: reservations){
+                if (reservation.getEndDate().isBefore(dateEnd) && reservation.getStartDate().isAfter(dateStart)){
+                    codes.add(2);
+                    return;
+                }
+            }
+        } catch (Exception ignored) {}
+    }
 }
