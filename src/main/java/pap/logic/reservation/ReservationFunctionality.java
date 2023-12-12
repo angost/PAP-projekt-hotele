98dcc3b664b5241adebfe493263e4ef890f8ed6c
Kacper Straszak <01178602@pw.edu.pl>
2023-12-12 12:39:50 +0100

    database update after changes

--- src/main/java/pap/logic/reservation/ReservationFunctionality.java
+++ src/main/java/pap/logic/reservation/ReservationFunctionality.java
@@ -19,6 +19,7 @@ public class ReservationFunctionality {
     public void changeStartDate(LocalDate startDate){
         if (startDate.isBefore(reservation.getEndDate())){
             reservation.setStartDate(startDate);
+            new ReservationDAO().update(reservation);
         }
         else {
             //TODO
@@ -28,6 +29,7 @@ public class ReservationFunctionality {
     public void changeEndDate(LocalDate endDate){
         if (endDate.isAfter(reservation.getStartDate())){
             reservation.setEndDate(endDate);
+            new ReservationDAO().update(reservation);
         }
         else {
             //TODO
@@ -36,17 +38,21 @@ public class ReservationFunctionality {
 
     public void changeDescription(String description){
         reservation.setDescription(description);
+        new ReservationDAO().update(reservation);
     }
 
     public void changePaidAmount(float paidAmount){
         reservation.setPaidAmount(paidAmount);
+        new ReservationDAO().update(reservation);
     }
 
     public void changeStatus(String status){
         reservation.setStatus(status);
+        new ReservationDAO().update(reservation);
     }
 
     public void changeIsPaid(boolean isPaid){
         reservation.setPaid(isPaid);
+        new ReservationDAO().update(reservation);
     }
 }
