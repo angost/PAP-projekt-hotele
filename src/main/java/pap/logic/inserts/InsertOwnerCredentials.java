cdcbbb5c035c5b08893de979c27cfe859283077c
Adrian Murawski <01178568@pw.edu.pl>
2023-12-12 01:30:28 +0100

    insert owner credentials works

--- src/main/java/pap/logic/inserts/InsertOwnerCredentials.java
+++ src/main/java/pap/logic/inserts/InsertOwnerCredentials.java
@@ -1,24 +1,53 @@
 package pap.logic.inserts;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import pap.db.SessionFactoryMaker;
 import pap.db.entities.*;
-import java.util.*;
+import pap.db.dao.OwnerDAO;
 
 
 public class InsertOwnerCredentials {
-//    private final Owner owner;
-//    private final Address ownerAddress;
+    private final Owner owner;
+    private Address ownerAddress;
+    boolean address;
 
     public InsertOwnerCredentials(String username, String password, String companyName, String email, String phoneNumber,
                                   String country, String city, String street, String postalCode, String streetNo,
-                                  String nip, boolean isVerified, boolean activeStatus, List <Hotel> hotels) {
-        // TODO
+                                  String nip, boolean isVerified, boolean activeStatus) {
+        ownerAddress = new Address();
+        ownerAddress.setCountry(country);
+        ownerAddress.setCity(city);
+        ownerAddress.setStreet(street);
+        ownerAddress.setStreetNumber(streetNo);
+        ownerAddress.setPostalCode(postalCode);
+        owner = new Owner();
+        owner.setUsername(username);
+        owner.setPassword(password);
+        owner.setEmail(email);
+        owner.setPhoneNumber(phoneNumber);
+        owner.setCompanyName(companyName);
+        owner.setNip(nip);
+        owner.setActive(isVerified);
+        owner.setVerified(activeStatus);
+        owner.setAddress(ownerAddress);
     }
 
-    public boolean insertIntoDatabase() {
-        // TODO
-        return true;
+    public InsertOwnerCredentials(String username, String password, String companyName, String email, String phoneNumber,
+                                  String nip, boolean isVerified, boolean activeStatus) {
+        owner = new Owner();
+        owner.setUsername(username);
+        owner.setPassword(password);
+        owner.setEmail(email);
+        owner.setPhoneNumber(phoneNumber);
+        owner.setCompanyName(companyName);
+        owner.setNip(nip);
+        owner.setActive(isVerified);
+        owner.setVerified(activeStatus);
+    }
+
+    public void insertIntoDatabase() {
+        if (address) {
+            new OwnerDAO().createWithNewAddress(owner);
+        } else {
+            new OwnerDAO().create(owner);
+        }
     }
 }
