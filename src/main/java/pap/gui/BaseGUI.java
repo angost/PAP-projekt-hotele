fd86daa33be143e96d1033f9452bf5325f868694
aostrows <01178572@pw.edu.pl>
2023-12-12 23:27:27 +0100

    Added logging out

--- src/main/java/pap/gui/BaseGUI.java
+++ src/main/java/pap/gui/BaseGUI.java
@@ -214,6 +214,21 @@ class UndoButton extends JButton{
     }
 }
 
+class LogOutButton extends JButton{
+    public LogOutButton(int buttonWidth, int buttonHeight, int imgWidth, int imgHeight) {
+        setPreferredSize(new Dimension(buttonWidth, buttonHeight));
+        setMaximumSize(new Dimension(buttonWidth, buttonHeight));
+        try {
+            setContentAreaFilled(false); // Make the button transparent
+            Image baseImg = ImageIO.read(new File(getClass().getResource("/logout_img_64.png").getPath()));
+            baseImg = baseImg.getScaledInstance(imgWidth, imgHeight, Image.SCALE_SMOOTH);
+            setIcon(new ImageIcon(baseImg));
+        } catch (Exception ex) {
+            setContentAreaFilled(true);
+        }
+    }
+}
+
 
 class UndoPanel extends JPanel{
     public UndoPanel(JPanel mainPanel, int frameWidth, int btnHeight, Color bgColor, ActionListener actionListener) {
