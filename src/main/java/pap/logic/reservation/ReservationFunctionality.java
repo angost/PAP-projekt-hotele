c2e538e2a70e5ed09c98a04713c6a6d26eb85c2f
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2023-12-12 16:17:39 +0100

    kstrasza commits from branch "logic_am"

---
+++ src/main/java/pap/logic/reservation/ReservationFunctionality.java
@@ -0,0 +1,70 @@
+package pap.logic.reservation;
+import pap.db.entities.*;
+import pap.db.dao.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ReservationFunctionality {
+
+    private final Reservation reservation;
+
+    public ReservationFunctionality(Integer resId){
+        reservation = new ReservationDAO().findById(resId);
+    }
+    public void deleteReservation(){
+        new ReservationDAO().delete(reservation);
+    }
+
+    public void changeStartDate(LocalDate startDate){
+        if (startDate.isBefore(reservation.getEndDate())){
+            reservation.setStartDate(startDate);
+            new ReservationDAO().update(reservation);
+        }
+        else {
+            //TODO
+        }
+    }
+
+    public void changeEndDate(LocalDate endDate){
+        if (endDate.isAfter(reservation.getStartDate())){
+            reservation.setEndDate(endDate);
+            new ReservationDAO().update(reservation);
+        }
+        else {
+            //TODO
+        }
+    }
+
+    public void changeDescription(String description){
+        reservation.setDescription(description);
+        new ReservationDAO().update(reservation);
+    }
+
+    public void changePaidAmount(Float paidAmount){
+        reservation.setPaidAmount(paidAmount);
+        new ReservationDAO().update(reservation);
+    }
+
+    public void changeStatus(String status){
+        reservation.setStatus(status);
+        new ReservationDAO().update(reservation);
+    }
+
+    public void changeIsPaid(Boolean isPaid){
+        reservation.setPaid(isPaid);
+        new ReservationDAO().update(reservation);
+    }
+
+    public void setNewPenalties(String reason, Integer amount, Boolean isPaid){
+        List<Penalty> penalties = reservation.getPenalties();
+        Penalty penalty = new Penalty();
+        penalty.setReason(reason);
+        penalty.setAmount(amount);
+        penalty.setPaid(isPaid);
+        penalty.setReservation(reservation);
+        penalties.add(penalty);
+        reservation.setPenalties(penalties);
+        new ReservationDAO().update(reservation);
+    }
+}
