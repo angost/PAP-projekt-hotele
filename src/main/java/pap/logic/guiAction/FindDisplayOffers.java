c26c38d2eec14af7e0003237f855aa87f2ca1a39
Angelika Ostrowska <01178572@pw.edu.pl>
2023-12-17 00:54:11 +0100

    Fixed logic function to get offer by offer_id not by position on the list

--- src/main/java/pap/logic/guiAction/FindDisplayOffers.java
+++ src/main/java/pap/logic/guiAction/FindDisplayOffers.java
@@ -2,7 +2,6 @@ package pap.logic.guiAction;
 
 import pap.db.entities.Offer;
 import pap.logic.SearchOffers;
-
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,8 +16,8 @@ public class FindDisplayOffers {
     public Integer[] getFittingElementsIds() {
         int offersCount = offers.size();
         Integer[] ids = new Integer[offersCount];
-        for (int i = 1; i <= offersCount; i++) {
-            ids[i-1] = i-1;
+        for (int i = 0; i < offersCount; i++) {
+            ids[i] = offers.get(i).getOfferId();
         }
         return ids;
     }
@@ -31,7 +30,14 @@ public class FindDisplayOffers {
     public HashMap<String, String> getElementInfo(int id){
         HashMap<String, String> elInfo = new HashMap<String, String>();
 
-        Offer offer = offers.get(id);
+        Offer offer = offers.get(0);
+        for (Offer nextOffer : offers){
+            if(nextOffer.getOfferId() == id) {
+                offer = nextOffer;
+                break;
+            }
+        }
+
         elInfo.put("name", offer.getName());
         elInfo.put("info", "room type: %s, rooms: %d, bathrooms: %d"
                 .formatted(offer.getRoomType(), offer.getRoomNumber(), offer.getBathroomNumber()));
