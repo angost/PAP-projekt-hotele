8afbaf499d7f39c61433b0fe0c3f0fe7a5b8d070
Adrian Murawski <01178568@pw.edu.pl>
2024-01-10 15:39:31 +0100

    discount codes fix

--- src/main/java/pap/logic/discountCodes/GetDiscountCodes.java
+++ src/main/java/pap/logic/discountCodes/GetDiscountCodes.java
@@ -6,6 +6,7 @@ import pap.db.SessionFactoryMaker;
 import pap.db.dao.DiscountDAO;
 import pap.db.entities.*;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.List;
 
 
@@ -17,7 +18,8 @@ public class GetDiscountCodes {
 
     public List<Discount> getDiscountsForOffer(Offer offer) {
         List <Discount> discounts;
-        String query = "SELECT discounts.* FROM discounts JOIN offers ON discounts.hotel_id = offers.hotel_id WHERE offers.offer_id = :offer_id";
+        String query = "SELECT discounts.* FROM discounts JOIN offers ON discounts.hotel_id = offers.hotel_id WHERE offers.offer_id = :offer_id AND discounts.is_active = true" +
+                " union SELECT * FROM discounts where hotel_id = 0 AND is_active = true";
         try (Session session = factory.openSession()) {
             discounts = session.createNativeQuery(query, Discount.class)
                     .setParameter("offer_id", offer.getOfferId())
@@ -28,7 +30,7 @@ public class GetDiscountCodes {
 
     public List <Discount> getDiscountsForHotel(Hotel hotel) {
         List <Discount> discounts;
-        String query = "SELECT * FROM discounts WHERE hotel_id = :hotel_id";
+        String query = "SELECT * FROM discounts WHERE hotel_id IN (:hotel_id, 0) AND is_active = true";
         try (Session session = factory.openSession()) {
             discounts = session.createNativeQuery(query, Discount.class)
                     .setParameter("hotel_id", hotel.getHotelId())
