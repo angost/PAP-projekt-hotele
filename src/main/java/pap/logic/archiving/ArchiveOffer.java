32208c89ee635ac3977f2c8239bc103ff93e6763
Adrian Murawski <01178568@pw.edu.pl>
2024-01-10 23:07:27 +0100

    checking implemented

--- src/main/java/pap/logic/archiving/ArchiveOffer.java
+++ src/main/java/pap/logic/archiving/ArchiveOffer.java
@@ -1,9 +1,16 @@
 package pap.logic.archiving;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import pap.db.SessionFactoryMaker;
 import pap.db.dao.OfferDAO;
 import pap.db.entities.Offer;
+import pap.db.entities.Reservation;
+
+import java.util.List;
 
 public class ArchiveOffer {
+    SessionFactory factory = SessionFactoryMaker.getFactory();
     private final Offer offer;
     public ArchiveOffer(Offer offer) {
         this.offer = offer;
@@ -11,6 +18,12 @@ public class ArchiveOffer {
 
     public boolean archive() {
         // checking
+        try (Session session = factory.openSession()) {
+            List<Reservation> reservationList = session.createNativeQuery("select * from reservations where LOWER(status) = 'active' and offer_id = " + offer.getOfferId(), Reservation.class).list();
+            if (!reservationList.isEmpty()) {
+                return false;
+            }
+        }
         offer.setActive(false);
         new OfferDAO().update(offer);
         return true;
