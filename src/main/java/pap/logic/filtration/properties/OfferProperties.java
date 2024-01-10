1838d313ca009889f20d72a80f21a2f4a9b48a60
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2024-01-10 20:54:16 +0100

    Assigned real values to GetOfferDetails

--- src/main/java/pap/logic/filtration/properties/OfferProperties.java
+++ src/main/java/pap/logic/filtration/properties/OfferProperties.java
@@ -13,6 +13,9 @@ public class OfferProperties extends Properties {
     private final Integer bedNoLowerBound;
     private final Integer bedNoUpperBound;
     private final Boolean hasKitchen;
+    private final Boolean hasWifi;
+    private final Boolean smokingAllowed;
+    private final Boolean hasParking;
     private final Boolean petFriendly;
     private final Integer priceLowerBound;
     private final Integer priceUpperBound;
@@ -22,6 +25,7 @@ public class OfferProperties extends Properties {
                            Integer roomNoLowerBound, Integer roomNoUpperBound,
                            Integer bedNoLowerBound, Integer bedNoUpperBound,
                            Boolean hasKitchen, Boolean petFriendly,
+                           Boolean hasWifi, Boolean smokingAllowed, Boolean hasParking,
                            Integer priceLowerBound, Integer priceUpperBound) {
         this.roomType = trimIfNotNull(roomType);
         this.name = trimIfNotNull(name);
@@ -33,6 +37,9 @@ public class OfferProperties extends Properties {
         this.bedNoUpperBound = bedNoUpperBound;
         this.hasKitchen = hasKitchen;
         this.petFriendly = petFriendly;
+        this.hasWifi = hasWifi;
+        this.smokingAllowed = smokingAllowed;
+        this.hasParking = hasParking;
         this.priceUpperBound = priceUpperBound;
         this.priceLowerBound = priceLowerBound;
     }
--- src/main/java/pap/logic/guiAction/OfferDetails.java
+++ src/main/java/pap/logic/guiAction/OfferDetails.java
@@ -1,25 +1,14 @@
 package pap.logic.guiAction;
 
 import pap.db.dao.OfferDAO;
+import pap.db.dao.RatingDAO;
 import pap.db.entities.Offer;
-import pap.db.entities.Rating;
-import pap.logic.RatingMean;
 
 import java.util.HashMap;
-import java.util.List;
 
 public class OfferDetails {
-    private String getImgPath(Integer offerId) {
-        HashMap<Integer, String> imgPathMap = new HashMap<Integer, String>();
-        imgPathMap.put(1, "/room1.jpg"); imgPathMap.put(2, "/room2.jpg");
-        imgPathMap.put(3, "/room3.jpg"); imgPathMap.put(4, "/room4.jpg");
-        imgPathMap.put(5, "/room5.jpg"); imgPathMap.put(6, "/room6.jpeg");
-        imgPathMap.put(7, "/room7.jpg"); imgPathMap.put(8, "/room8.jpg");
-        imgPathMap.put(9, "/room9.jpeg"); imgPathMap.put(10, "/room10.jpg");
-        return imgPathMap.get(offerId);
-    }
     public HashMap<String, String> getOfferInfo(Integer offerId) {
-        HashMap<String, String> offerInfo = new HashMap<String, String>();
+        HashMap<String, String> offerInfo = new HashMap<>();
         OfferDAO od = new OfferDAO();
         Offer offer = od.findById(offerId);
 
@@ -27,7 +16,6 @@ public class OfferDetails {
         offerInfo.put("hotel", offer.getHotel().getName());
         offerInfo.put("price", String.format("%.2f", offer.getPrice()) + " PLN");
         offerInfo.put("description", offer.getDescription());
-        offerInfo.put("img_path", getImgPath(offerId));
         offerInfo.put("street", offer.getHotel().getAddress().getStreet());
         offerInfo.put("street_nr", offer.getHotel().getAddress().getStreetNumber());
         offerInfo.put("city", offer.getHotel().getAddress().getCity());
@@ -35,12 +23,33 @@ public class OfferDetails {
         offerInfo.put("room_type", offer.getRoomType());
         offerInfo.put("rooms_nr", String.valueOf(offer.getRoomNumber()));
         offerInfo.put("bathrooms_nr", String.valueOf(offer.getBathroomNumber()));
-        List<Rating> ratings = offer.getRatings(); //TODO
-        offerInfo.put("review_score", "4.12"/*String.valueOf(RatingMean.calculateMean(ratings))*/);
-        offerInfo.put("reviews_nr", "22" /*String.valueOf(ratings.size())*/);
-        offerInfo.put("facilities_yes", "Free Wi-Fi:  ✔    Air conditioning:  ✔    TV in room:  ✔    Breakfast included:  ✔    24/7 reception:  ✔    Laundry services:  ✔   Airport transfer:  ✔    Room service:  ✔    Meeting/conference rooms:  ✔    Family-friendly:  ✔    Close to city center:  ✔");
-        offerInfo.put("facilities_no", "Parking availability:  ✘    Disability access:  ✘    Balcony:  ✘    Pet-friendly:  ✘    Gym access:  ✘    Security features:  ✘        Kitchen:  ✘    Private bathroom:  ✘    Smoking allowed:  ✘    Pool access:  ✘");
-        //✘✖
+        offerInfo.put("people_nr", String.valueOf(offer.getBedNumber()));
+        offerInfo.put("review_score", String.valueOf(new RatingDAO().getAverageOfferRating(offerId)));
+        offerInfo.put("reviews_nr", String.valueOf(new RatingDAO().getOfferRatingsAmount(offerId)));
+        String yes = "";
+        String no = "";
+        if (offer.isHasKitchen())
+            yes += "Kitchen:  ✔    ";
+        else
+            no += "Kitchen:  ✘    ";
+        if (offer.isPetFriendly())
+            yes += "Pet-friendly:  ✔    ";
+        else
+            no += "Pet-friendly:  ✘    ";
+        if (offer.isHasWifi())
+            yes += "Free Wi-Fi:  ✔    ";
+        else
+            no += "Free Wi-Fi:  ✘    ";
+        if (offer.isSmokingAllowed())
+            yes += "Smoking allowed:  ✔    ";
+        else
+            no += "Smoking allowed:  ✘    ";
+        if (offer.isHasParking())
+            yes += "Parking available:  ✔    ";
+        else
+            no += "Parking available:  ✘    ";
+        offerInfo.put("facilities_yes", yes);
+        offerInfo.put("facilities_no", no);
         return offerInfo;
     }
 }
