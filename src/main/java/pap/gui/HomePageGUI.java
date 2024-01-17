4d5286d2318683cfcfd5f489667a46bd1d804495
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2024-01-17 02:20:02 +0100

    Added changes for AdminGUI which got lost and hooked action to discount buttons

--- src/main/java/pap/gui/HomePageGUI.java
+++ src/main/java/pap/gui/HomePageGUI.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import pap.db.dao.ClientDAO;
 import pap.db.dao.OwnerDAO;
+import pap.db.dao.AdminDAO;
 import pap.gui.components.LogOutButton;
 import pap.gui.components.LogoPanel;
 import pap.gui.components.TextIconButton;
@@ -57,7 +58,11 @@ public class HomePageGUI extends BaseGUI {
             OwnerDAO od = new OwnerDAO();
             name = od.findById(userId).getCompanyName();
             userImgPath = "/business.png";
-        } else {
+        } else if (userType.equals("Admin")) {
+            AdminDAO ad = new AdminDAO();
+            name = ad.findById(userId).getName();
+            userImgPath = "/admin.png";
+            } else {
             name = "User";
         }
         welcomeLabel = new JLabel("Hello " + name + "!", JLabel.CENTER);
@@ -94,8 +99,7 @@ public class HomePageGUI extends BaseGUI {
         buttonsPanel.add(Box.createRigidArea(new Dimension(menuButtonGap,0)));
         buttonsPanel.add(buttonsRowsPanel);
 
-        if (userType.equals("Client")){
-
+        if (userType.equals("Client")) {
             JPanel buttonsRow1 = new JPanel();
             buttonsRow1.setLayout(new BoxLayout(buttonsRow1, BoxLayout.LINE_AXIS));
             buttonsRow1.setBackground(bgColor);
@@ -131,7 +135,8 @@ public class HomePageGUI extends BaseGUI {
             buttonsRowsPanel.add(buttonsRow1); buttonsRowsPanel.add(Box.createVerticalGlue());
             buttonsRowsPanel.add(buttonsRow2); buttonsRowsPanel.add(Box.createVerticalGlue());
             buttonsRowsPanel.add(buttonsRow3); buttonsRowsPanel.add(Box.createVerticalGlue());
-        } else if (userType.equals("Owner")) {
+        }
+        else if (userType.equals("Owner")) {
             JPanel buttonsRow1 = new JPanel();
             buttonsRow1.setLayout(new BoxLayout(buttonsRow1, BoxLayout.LINE_AXIS));
             buttonsRow1.setBackground(bgColor);
@@ -164,6 +169,7 @@ public class HomePageGUI extends BaseGUI {
             buttonsRow4.setLayout(new BoxLayout(buttonsRow4, BoxLayout.LINE_AXIS));
             buttonsRow4.setBackground(bgColor);
             discountsButton = new MenuButton("Discount codes", "/icons/discount.png");
+            discountsButton.addActionListener(e->seeDiscountsAction());
             deactivateAccountButton = new MenuButton("Deactivate account", "/icons/deactivate.png");
             deactivateAccountButton.fillColor = statusWrongLighter;
             deactivateAccountButton.hoverColor = statusWrong;
@@ -176,8 +182,17 @@ public class HomePageGUI extends BaseGUI {
             buttonsRowsPanel.add(buttonsRow3); buttonsRowsPanel.add(Box.createVerticalGlue());
             buttonsRowsPanel.add(buttonsRow4); buttonsRowsPanel.add(Box.createVerticalGlue());
         }
+        else if (userType.equals("Admin")) {
+            JPanel buttonsRow1 = new JPanel();
+            buttonsRow1.setLayout(new BoxLayout(buttonsRow1, BoxLayout.LINE_AXIS));
+            buttonsRow1.setBackground(bgColor);
+            discountsButton = new MenuButton("Manage discount codes", "/icons/discount.png");
+            discountsButton.addActionListener(e->seeDiscountsAction());
+            buttonsRow1.add(discountsButton); buttonsRow1.add(Box.createHorizontalGlue());
+            buttonsRowsPanel.add(buttonsRow1); buttonsRowsPanel.add(Box.createVerticalGlue());
+        }
 
-        mainPanel.add(buttonsPanel);
+            mainPanel.add(buttonsPanel);
         mainPanel.add(Box.createVerticalGlue());
     }
 
@@ -257,8 +272,8 @@ public class HomePageGUI extends BaseGUI {
     }
 
     void seeDiscountsAction(){
-        //new ScrollDiscountsGUI(userId, userType).createGUI();
-        //frame.setVisible(false);
+        new ScrollDiscountsGUI(userId, userType).createGUI();
+        frame.setVisible(false);
     }
 
     void addHotelAction(){
